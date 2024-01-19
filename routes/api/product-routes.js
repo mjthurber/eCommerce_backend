46e432df9cef2620cb6e06f90b21f94d26ec80f8
mjthurber <mjthurber@gmail.com>
2024-01-19 14:44:07 -0700

    finished insomnia bugs

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }],
+    });
+    res.json(products);
+  }
   // be sure to include its associated Category and Tag data
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }],
+    });
   // be sure to include its associated Category and Tag data
-});
+
+  if (!product) {
+    res.status(404).json({ message: 'No product found with this id!' });
+    return;
+  }
+  res.json(product);
+}
+    
+  catch (err) {
+    res.status(500).json(err);
+  }
+  });
 
 // create new product
 router.post('/', (req, res) => {
@@ -92,7 +116,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+  if (!product) {
+    res.status(404).json({ message: 'No product found with this id!' });
+    return;
+  }
+  res.json(product);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+
   // delete one product by its `id` value
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,25 +3,90 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'tag_products' }],
+    });
+    res.json(tags);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tag_products' }],
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.json(tag);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const tag = await Tag.create(req.body);
+    res.status(200).json(tag);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tag);
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    await ProductTag.destroy({
+      where: {
+        tag_id: req.params.id,
+      },
+    });
+
+    res.json(tag);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
